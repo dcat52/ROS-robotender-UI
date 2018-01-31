1ca9c4dfba9760922c4db67bcb4f53e5595cbba9
Shannon Enders <shannon.enders.14@cnu.edu>
2018-01-31 13:13:49 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roboTenderUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the roboTenderUiApp
+ */
+angular.module('roboTenderUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roboTenderUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the roboTenderUiApp
+ */
+angular.module('roboTenderUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
